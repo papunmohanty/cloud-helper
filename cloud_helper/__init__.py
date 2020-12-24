597220fb652eaf36391448fb9e57b0625988e916
papunmohanty <papunmohanty31@gmail.com>
2020-12-24 22:54:25 +0530

    Fix minor version change

--- cloud_helper/__init__.py
+++ cloud_helper/__init__.py
@@ -1,2 +1,2 @@
 # Version of the cloud-helper package
-__version__ = "1.0.0"
+__version__ = "1.0.1"
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="cloud-helper",
-    version="1.0.0",
+    version="1.0.1",
     description="A convenient Helper library for various cloud services",
     long_description=README,
     long_description_content_type="text/markdown",
